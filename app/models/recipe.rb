4ccae37b0b292b358160b03cbcba6bb42828eb69
Joe Brown <joebrown83@gmail.com>
2013-07-19 21:13:03 -0500

    commit for public repo

---
+++ app/models/recipe.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+#require 'httparty'
+
+class Recipe
+  include HTTParty
+
+  base_uri 'www.recipepuppy.com'
+  default_params output: "json", onlyImages: 1
+  format :json
+
+  def self.for(text)
+    get("/api", :query => {:q => text})["results"]
+  end
+end
+
+#puts Recipe.for "chocolate"
+#puts Recipe.for "apple pie"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Facecook::Application.config.secret_token = '8c8e6befb8d869153c3bf0f2aa8a5bcc729a4a6e47dfb54f4129278df0878e2338c48269b2f08e743468e66dc6c6c37d142ed07db2b82d102feec3316eda51c5'
